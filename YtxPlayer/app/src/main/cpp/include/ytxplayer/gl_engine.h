0ec88ec9163de4fa8389aaf59a2f8a86b76f2c20
YuHao <yu.hao@xiaoyi.com>
2016-11-24 18:19:02 +0800

    整理代码

--- YtxPlayer/app/src/main/cpp/include/ytxplayer/gl_engine.h
+++ YtxPlayer/app/src/main/cpp/include/ytxplayer/gl_engine.h
@@ -19,81 +19,9 @@
 #include "lock.h"
 
 
-
 class GlEngine {
 
 private:
-
-//    char *gVertexShader = "attribute vec4 vPosition;\n"
-//            "void main() {\n"
-//            "  gl_Position = vPosition;\n"
-//            "}\n";
-//
-//    char *gFragmentShader = "precision mediump float;\n"
-//            "void main() {\n"
-//            "  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
-//            "}\n";
-
-
-//    "a_texCoord" //texcoord 是纹理坐标，在后续的Pixel shader中会用到用来读取纹理颜色
-//    GLfloat coord_buffer[8] = {
-//            0.0f, 1.0f,
-//            1.0f, 1.0f,
-//            0.0f, 0.0f,
-//            1.0f, 0.0f,
-//    };
-
-//    "vPosition"
-//    GLfloat vertice_buffer[8] = {
-//            -1.0f, -1.0f,
-//            1.0f, -1.0f,
-//            -1.0f, 1.0f,
-//            1.0f, 1.0f,
-//    };
-
-//    Y’ = 0.257*R' + 0.504*G' + 0.098*B' + 16
-//    Cb' = -0.148*R' - 0.291*G' + 0.439*B' + 128
-//    Cr' = 0.439*R' - 0.368*G' - 0.071*B' + 128
-//    R' = 1.164*(Y’-16) + 1.596*(Cr'-128)
-//    G' = 1.164*(Y’-16) - 0.813*(Cr'-128) - 0.392*(Cb'-128)
-//    B' = 1.164*(Y’-16) + 2.017*(Cb'-128)
-
-  char* gVertexShader =
-                    "attribute vec4 vPosition;\n"
-                    "attribute vec2 a_texCoord;\n"
-                    "varying vec2 tc;\n"
-                    "void main() {\n"
-                    "gl_Position = vPosition;\n"
-                    "tc = a_texCoord;\n"
-                    "}\n";
-
-   char* gFragmentShader =
-                    "precision mediump float;\n"
-                    "uniform sampler2D tex_y;\n"
-                    "uniform sampler2D tex_u;\n"
-                    "uniform sampler2D tex_v;\n"
-                    "varying vec2 tc;\n"
-                    "void main() {\n"
-                    "vec4 c = vec4((texture2D(tex_y, tc).r - 16./255.) * 1.164);\n"
-                    "vec4 U = vec4(texture2D(tex_u, tc).r - 128./255.);\n"
-                    "vec4 V = vec4(texture2D(tex_v, tc).r - 128./255.);\n"
-                    "c += V * vec4(1.596, -0.813, 0, 0);\n"
-                    "c += U * vec4(0, -0.392, 2.017, 0);\n"
-                    "c.a = 1.0;\n"
-                    "gl_FragColor = c;\n"
-                    "}\n";
-
-//    vec3 yuv;
-//    vec3 rgb;
-//    yuv.x = texture2D(tex_y, textureOut).r;
-//    yuv.y = texture2D(tex_u, textureOut).r - 0.5;
-//    yuv.z = texture2D(tex_v, textureOut).r - 0.5;
-//    rgb = mat3( 1,       1,         1,
-//                0,       -0.39465,  2.03211,
-//                1.13983, -0.58060,  0) * yuv;
-//    gl_FragColor = vec4(rgb, 1);
-
-
 //    char* gFragmentShader =
 //            "precision mediump float;\n"
 //                    "uniform sampler2D tex_y;\n"
@@ -111,10 +39,43 @@ private:
 //                    "}\n";
 
 
+//    Y’ = 0.257*R' + 0.504*G' + 0.098*B' + 16
+//    Cb' = -0.148*R' - 0.291*G' + 0.439*B' + 128
+//    Cr' = 0.439*R' - 0.368*G' - 0.071*B' + 128
+//    R' = 1.164*(Y’-16) + 1.596*(Cr'-128)
+//    G' = 1.164*(Y’-16) - 0.813*(Cr'-128) - 0.392*(Cb'-128)
+//    B' = 1.164*(Y’-16) + 2.017*(Cb'-128)
+
+    char *gVertexShader =
+            "attribute vec4 vPosition;\n"
+                    "attribute vec2 a_texCoord;\n"
+                    "varying vec2 tc;\n"
+                    "void main() {\n"
+                    "gl_Position = vPosition;\n"
+                    "tc = a_texCoord;\n"
+                    "}\n";
+
+    char *gFragmentShader =
+            "precision mediump float;\n"
+                    "uniform sampler2D tex_y;\n"
+                    "uniform sampler2D tex_u;\n"
+                    "uniform sampler2D tex_v;\n"
+                    "varying vec2 tc;\n"
+                    "void main() {\n"
+                    "vec4 c = vec4((texture2D(tex_y, tc).r - 16./255.) * 1.164);\n"
+                    "vec4 U = vec4(texture2D(tex_u, tc).r - 128./255.);\n"
+                    "vec4 V = vec4(texture2D(tex_v, tc).r - 128./255.);\n"
+                    "c += V * vec4(1.596, -0.813, 0, 0);\n"
+                    "c += U * vec4(0, -0.392, 2.017, 0);\n"
+                    "c.a = 1.0;\n"
+                    "gl_FragColor = c;\n"
+                    "}\n";
+
 
     GLfloat gTriangleVertices[6] = {0.0f, 0.5f, -0.5f, -0.5f,
                                     0.5f, -0.5f};
 
+    //    "a_texCoord" //texcoord 是纹理坐标，在后续的Pixel shader中会用到用来读取纹理颜色
     GLfloat coord_buffer[8] = {
             0.0f, 1.0f,
             1.0f, 1.0f,
@@ -122,6 +83,7 @@ private:
             1.0f, 0.0f,
     };
 
+    //    "vPosition"
     GLfloat vertice_buffer[8] = {
             -1.0f, -1.0f,
             1.0f, -1.0f,
@@ -136,7 +98,7 @@ private:
     int videoWidth = 0;
     int videoHeight = 0;
     int mScreenWidth = 720;
-    int mScreenHeight= 1080;
+    int mScreenHeight = 1080;
 
     GLuint program;
     GLuint gProgram;
@@ -149,18 +111,23 @@ private:
 
 protected:
     GlEngine();
-    ~GlEngine(){
-        if(glEngine != NULL){
+
+    ~GlEngine() {
+        if (glEngine != NULL) {
             delete glEngine;
         }
     }
 
 public:
     static GlEngine *getGlEngine();
-    static bool glEngineInitComplete(){
+
+    static bool glEngineInitComplete() {
         return GlEngine::isInitComplete;
     }
 
+    static void glSetEngineInitComplete(bool isComplete) {
+        GlEngine::isInitComplete = isComplete;
+    }
 
     void printGLString(const char *name, GLenum s);
 
@@ -171,14 +138,21 @@ public:
     GLuint createProgram(const char *pVertexSource, const char *pFragmentSource);
 
     bool setupGraphics();
+
     bool isSetupGraphics;
 
-    void buildTextures(char *y, char *u, char *v, int videoWidth, int videoHeight);
+    void buildTextures();
 
     void drawFrame();
 
+    void drawFrameInit(int videoWidth, int videoHeight);
+
+    bool isDrawFrameInit;
+
     void addRendererFrame(char *y, char *u, char *v, int videoWidth, int videoHeight);
+
     bool isAddRendererFrameInit;
+
     void addRendererFrameInit(int videoWidth, int videoHeight);
 
     void setAspectRatio();
--- YtxPlayer/app/src/main/cpp/src/gl_engine.cpp
+++ YtxPlayer/app/src/main/cpp/src/gl_engine.cpp
@@ -5,14 +5,15 @@
 
 GlEngine::GlEngine() {
 
-    yTextureId =  255;
-    uTextureId =  255;
-    vTextureId =  255;
+//    yTextureId = 255;
+//    uTextureId = 255;
+//    vTextureId = 255;
     yHandle = -1;
     uHandle = -1;
     vHandle = -1;
     isAddRendererFrameInit = false;
     isSetupGraphics = false;
+    isDrawFrameInit = false;
 
 
 }
@@ -98,6 +99,7 @@ GLuint GlEngine::createProgram(const char *pVertexSource, const char *pFragmentS
 
 
 bool GlEngine::setupGraphics() {
+    GLint ret = -1;
     printGLString("Version", GL_VERSION);
     printGLString("Vendor", GL_VENDOR);
     printGLString("Renderer", GL_RENDERER);
@@ -113,19 +115,20 @@ bool GlEngine::setupGraphics() {
      * get handle for "vPosition" and "a_texCoord"
      */
 
-    gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");
-    checkGlError("glGetAttribLocation");
-    ALOGI("glGetAttribLocation(\"vPosition\") = %d\n", gvPositionHandle);
-    if (gvPositionHandle == -1) {
-        ALOGE("setupGraphics error gvPositionHandle =%d\n", gvPositionHandle);
+    ret = glGetAttribLocation(gProgram, "vPosition");
+    if (ret == -1) {
+        ALOGE("setupGraphics error glGetAttribLocation(gProgram, \"vPosition\") =%d\n", ret);
     }
-
-    gCoordHandle = glGetAttribLocation(gProgram, "a_texCoord");
     checkGlError("glGetAttribLocation");
+    gvPositionHandle = (GLuint) ret;
 
-    if (gCoordHandle == -1) {
-        ALOGE("setupGraphics error gCoordHandle = %d\n", gCoordHandle);
+
+    ret = glGetAttribLocation(gProgram, "a_texCoord");
+    if (ret == -1) {
+        ALOGE("setupGraphics error GetAttribLocation(gProgram, \"a_texCoord\") = %d\n", ret);
     }
+    checkGlError("glGetAttribLocation");
+    gCoordHandle = (GLuint) ret;
 
     /*
      * get uniform location for y/u/v, we pass data through these uniforms
@@ -152,10 +155,10 @@ bool GlEngine::setupGraphics() {
         ALOGE("Could not get uniform location for tex_v");
     }
 
-//    glUseProgram(program);
-//    checkGlError("glUseProgram");
-    // glViewport(0, 0, w, h);
-    // checkGlError("glViewport");
+    glUseProgram(program);
+    checkGlError("glUseProgram");
+
+    isSetupGraphics = true;
     return true;
 }
 
@@ -164,18 +167,17 @@ void GlEngine::setVideoWidthAndHeight(int videoWidth, int videoHeight) {
     this->videoHeight = videoHeight;
 }
 
-void GlEngine::buildTextures(char *y, char *u, char *v, int videoWidth, int videoHeight) {
+void GlEngine::buildTextures() {
     // building texture for Y data
-    if (yTextureId == 255) {
-        glGenTextures(1, &yTextureId);  //参数1:用来生成纹理的数量. 参数2:存储纹理索引的第一个元素指针
-        checkGlError("glGenTextures");
-        ALOGI("buildTextures yTextureId=%d\n", yTextureId);
-    }
+
+    glGenTextures(1, &yTextureId);  //参数1:用来生成纹理的数量. 参数2:存储纹理索引的第一个元素指针
+    checkGlError("glGenTextures");
+    ALOGI("buildTextures yTextureId=%d\n", yTextureId);
+
     glBindTexture(GL_TEXTURE_2D, yTextureId);
     checkGlError("glBindTexture");
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, videoWidth, videoHeight, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, y);
-    checkGlError("glTexImage2D");
-  //  设置纹理参数,缩小过滤,邻近像素差值(临近像素插值。该方法质量较差)
+
+    //  设置纹理参数,缩小过滤,邻近像素差值(临近像素插值。该方法质量较差)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     //设置纹理参数,放大过滤,线性插值(线性插值。使用距离当前渲染像素中心最近的4个纹素加权平均值)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -183,29 +185,27 @@ void GlEngine::buildTextures(char *y, char *u, char *v, int videoWidth, int vide
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     // building texture for U data
-    if (uTextureId == 255) {
-        glGenTextures(1, &uTextureId);  //参数1:用来生成纹理的数量. 参数2:存储纹理索引的第一个元素指针
-        checkGlError("glGenTextures");
-        ALOGI("buildTextures uTextureId=%d\n", uTextureId);
-    }
+
+    glGenTextures(1, &uTextureId);  //参数1:用来生成纹理的数量. 参数2:存储纹理索引的第一个元素指针
+    checkGlError("glGenTextures");
+    ALOGI("buildTextures uTextureId=%d\n", uTextureId);
+
     glBindTexture(GL_TEXTURE_2D, uTextureId);
     checkGlError("glBindTexture");
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, videoWidth / 2, videoHeight / 2, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, u);
-    checkGlError("glTexImage2D");
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    if (vTextureId == 255) {
-        glGenTextures(1, &vTextureId);  //参数1:用来生成纹理的数量. 参数2:存储纹理索引的第一个元素指针
-        checkGlError("glGenTextures");
-        ALOGI("buildTextures vTextureId=%d\n", vTextureId);
-    }
+
+    glGenTextures(1, &vTextureId);  //参数1:用来生成纹理的数量. 参数2:存储纹理索引的第一个元素指针
+    checkGlError("glGenTextures");
+    ALOGI("buildTextures vTextureId=%d\n", vTextureId);
+
     glBindTexture(GL_TEXTURE_2D, vTextureId);
     checkGlError("glBindTexture");
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, videoWidth / 2, videoHeight / 2, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, v);
-    checkGlError("glTexImage2D");
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -214,48 +214,63 @@ void GlEngine::buildTextures(char *y, char *u, char *v, int videoWidth, int vide
 
 }
 
+void GlEngine::drawFrameInit(int videoWidth, int videoHeight) {
+    if (!isDrawFrameInit) {
+        isDrawFrameInit = true;
+        glViewport(0, 0, videoWidth, videoHeight);
+        checkGlError("glViewport");
+        buildTextures();
+    }
+}
 
 /**
  * render the frame
  * the YUV data will be converted to RGB by shader.
  */
 void GlEngine::drawFrame() {
-    if (plane[0] != NULL && plane[1] != NULL && plane[2] != NULL && videoWidth != 0 && videoHeight != 0) {
+    if (plane[0] != NULL && plane[1] != NULL && plane[2] != NULL && videoWidth != 0 &&
+        videoHeight != 0) {
 
-     //   setAspectRatio();
+        drawFrameInit(videoWidth, videoHeight);
+        //   setAspectRatio();
 
 
-       // glViewport(0, 0, videoWidth, videoHeight);
-       // checkGlError("glViewport");
-//        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT);
         ALOGI("drawFrame videoWidth=%d videoHeight=%d\n", videoWidth, videoHeight);
-        buildTextures(plane[0], plane[1], plane[2], videoWidth, videoHeight);
+
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(program);
-        checkGlError("glUseProgram");
-        glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, false, 8, vertice_buffer );
+        glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, false, 8, vertice_buffer);
         checkGlError("glVertexAttribPointer mPositionHandle");
         glEnableVertexAttribArray(gvPositionHandle);
 
-        glVertexAttribPointer(gCoordHandle, 2, GL_FLOAT, false, 8, coord_buffer );
+        glVertexAttribPointer(gCoordHandle, 2, GL_FLOAT, false, 8, coord_buffer);
         checkGlError("glVertexAttribPointer maTextureHandle");
         glEnableVertexAttribArray(gCoordHandle);
 
         // bind textures
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, yTextureId);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, videoWidth, videoHeight, 0, GL_LUMINANCE,
+                     GL_UNSIGNED_BYTE, plane[0]);
+        checkGlError("glTexImage2D");
         glUniform1i(yHandle, 0);
 
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, uTextureId);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, videoWidth / 2, videoHeight / 2, 0,
+                     GL_LUMINANCE,
+                     GL_UNSIGNED_BYTE, plane[1]);
+        checkGlError("glTexImage2D");
         glUniform1i(uHandle, 1);
 
         glActiveTexture(GL_TEXTURE2);
         glBindTexture(GL_TEXTURE_2D, vTextureId);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, videoWidth / 2, videoHeight / 2, 0,
+                     GL_LUMINANCE,
+                     GL_UNSIGNED_BYTE, plane[2]);
+        checkGlError("glTexImage2D");
         glUniform1i(vHandle, 2);
 
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
@@ -268,23 +283,14 @@ void GlEngine::drawFrame() {
 }
 
 void GlEngine::addRendererFrame(char *y, char *u, char *v, int videoWidth, int videoHeight) {
-    addRendererFrameInit(videoWidth,videoHeight);
+    addRendererFrameInit(videoWidth, videoHeight);
     memcpy(plane[0], y, (size_t) (videoWidth * videoHeight));
-    memcpy(plane[1],u,(size_t) (videoWidth * videoHeight)/4);
-    memcpy(plane[2],v,(size_t) (videoWidth * videoHeight)/4);
-
-    //memset(plane[0],1,(size_t) (videoWidth * videoHeight));
-//    memset(plane[1],255,(size_t) (videoWidth * videoHeight)/4);
-//    memset(plane[2],255,(size_t) (videoWidth * videoHeight)/4);
-//    plane[0] = y;
-//    plane[1] = u;
-//    plane[2] = v;
-//    this->videoWidth = videoWidth;
-//    this->videoHeight = videoHeight;
+    memcpy(plane[1], u, (size_t) (videoWidth * videoHeight) / 4);
+    memcpy(plane[2], v, (size_t) (videoWidth * videoHeight) / 4);
 }
 
 void GlEngine::addRendererFrameInit(int videoWidth, int videoHeight) {
-    if(!isAddRendererFrameInit) {
+    if (!isAddRendererFrameInit) {
         isAddRendererFrameInit = true;
         plane[0] = (char *) malloc(sizeof(char) * videoWidth * videoHeight);
         plane[1] = (char *) malloc(sizeof(char) * videoWidth * videoHeight / 4);
@@ -292,15 +298,15 @@ void GlEngine::addRendererFrameInit(int videoWidth, int videoHeight) {
     }
 }
 
-void GlEngine::setAspectRatio(){
+void GlEngine::setAspectRatio() {
     float f1 = (float) mScreenHeight / mScreenWidth;
-    float f2 = (float)  videoHeight / videoWidth;
+    float f2 = (float) videoHeight / videoWidth;
     float widthScale = 0.0;
     float heightScale = 0.0;
     if (f1 == f2) {
 
     } else if (f1 < f2) {
-        widthScale =  f1 / f2;
+        widthScale = f1 / f2;
         ALOGI("setAspectRatio widthScale=%f\n", widthScale);
         vertice_buffer[0] = -widthScale;
         vertice_buffer[1] = -1.0f;
@@ -336,7 +342,7 @@ GlEngine *GlEngine::getGlEngine() {
         mLock.lock();
         if (glEngine == NULL) {
             glEngine = new GlEngine();
-            GlEngine::isInitComplete = true;
+            GlEngine::glSetEngineInitComplete(true);
         }
         mLock.unlock();
     }
@@ -358,17 +364,15 @@ JNIEXPORT void JNICALL Java_com_ytx_ican_media_player_gl2jni_GL2JNILib_native_1i
 JNIEXPORT void JNICALL Java_com_ytx_ican_media_player_gl2jni_GL2JNILib_native_1resize_1opengl(
         JNIEnv *env, jclass clazz, jint width, jint height) {
     ALOGI("native_1resize_1opengl IN");
-    // setupGraphicsTest(width,height);
+
     ALOGI("native_1resize_1opengl OUT");
 }
 // onDrawFrame
 JNIEXPORT void JNICALL Java_com_ytx_ican_media_player_gl2jni_GL2JNILib_native_1step_1opengl(
         JNIEnv *env, jclass clazz) {
     ALOGI("native_1step_1opengl IN");
-    // renderFrameTest();
     GlEngine::getGlEngine()->drawFrame();
     ALOGI("native_1step_1opengl OUT");
-
 }
 
 // onSurfaceCreated
@@ -376,12 +380,9 @@ JNIEXPORT void JNICALL Java_com_ytx_ican_media_player_gl2jni_GL2JNILib_native_1i
         JNIEnv *env, jclass clazz) {
 
     ALOGI("native_1init_1opengl IN");
-    if(!GlEngine::getGlEngine()->isSetupGraphics){
-        GlEngine::getGlEngine()->isSetupGraphics = true;
+    if (!GlEngine::getGlEngine()->isSetupGraphics) {
         GlEngine::getGlEngine()->setupGraphics();
     }
-
-
     ALOGI("native_1init_1opengl OUT");
 
 }
